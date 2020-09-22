37ef03e9d2014672598b6ba6b22b0f983a727010
jkurambhatti <jkurambhatti2992@gmail.com>
2020-09-22 23:29:20 +0530

    Updates

---
+++ precache-manifest.1ed67cd8b3e61048200cbb594014c134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13d760741a7f5fc641b0eee5ebf10a3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b250a81480b2dbc8fb1",
+    "url": "/monsters-rolodex/static/css/main.12a8b00a.chunk.css"
+  },
+  {
+    "revision": "6537d1c36a886b946142",
+    "url": "/monsters-rolodex/static/js/2.9e6a8291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9e6a8291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b250a81480b2dbc8fb1",
+    "url": "/monsters-rolodex/static/js/main.22e82dfa.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
